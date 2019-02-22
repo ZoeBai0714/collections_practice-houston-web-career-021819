d97dac2d93980f3ffaa0be7a7bc0e2693b83734a
Zoe Bai <s.xue.bai@gmail.com>
2019-02-22 03:35:12 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,31 @@
+def sort_array_asc(numbers)
+ numbers.sort
+end
 
+
+def sort_array_desc(numbers)
+  numbers.sort{|a, b| -(a<=>b)}
+end
+
+def sort_array_char_count(strings)
+  strings.sort_by{|string| (string.length)}
+end
+
+ def swap_elements(array)
+ array[1], array[2] = array[2], array[1]
+ array
+end
+
+
+def reverse_array(numbers)
+  numbers.reverse
+end
+
+def kesha_maker(strings)
+  array = []
+  strings.each do |string|
+    string = string.replace(string[2],"$")
+    array << string
+  end
+  puts array
+end
